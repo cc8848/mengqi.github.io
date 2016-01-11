74629e7a8dc464e3015d300e94507f4c0b716a91
359776371@qq.com <359776371@qq.com>
2016-01-11 11:30:21 +0800

    aa

---
+++ js/getUsermedia.js
@@ -0,0 +1,28 @@
+
+var video = document.querySelector('video');
+var canvas = window.canvas = document.querySelector('canvas');
+canvas.width = 480;
+canvas.height = 360;
+
+var button = document.querySelector('button');
+button.onclick = function() {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+};
+
+var constraints = {
+  audio: false,
+  video: true
+};
+
+function successCallback(stream) {
+  window.stream = stream; // make stream available to browser console
+  video.srcObject = stream;
+}
+
+function errorCallback(error) {
+  console.log('navigator.getUserMedia error: ', error);
+}
+navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+navigator.getUserMedia(constraints, successCallback, errorCallback);
