e31df9676e7828577af6d198633c474754386384
359776371@qq.com <359776371@qq.com>
2016-01-16 13:59:09 +0800

    messge

--- js/getUsermedia.js
+++ js/getUsermedia.js
@@ -1,41 +1,40 @@
-alert(8888888)
-// window.addEventListener('messge',function(ev){
-//   console.log('ss')
-// },false);
-window.onmessage = function(){
-  alert(8)
+window.onmessage = function(ev){
+  if(ev.data = 'photo'){
+     openVideo();
+     window.parent.postMessage(imgdata.value,'http://localhost:8881/main.html')
+  }
 }
-// function openVideo(){
-//       var video = oIframe.contentWindow.document.querySelector('video');
-//       var canvas = window.canvas = oIframe.contentWindow.document.querySelector('canvas');
-//       var imgdata= oIframe.contentWindow.document.getElementById("imgdata");
-//       var button = oIframe.contentWindow.document.querySelector('button');
-//       var oDownload = oIframe.contentWindow.document.getElementById("download");
-//       var imgStr = '';
+function openVideo(){
+      var video = document.querySelector('video');
+      var canvas = window.canvas = document.querySelector('canvas');
+      var imgdata= document.getElementById("imgdata");
+      var button = document.querySelector('button');
+      var oDownload = document.getElementById("download");
+      var imgStr = '';
       
-//       canvas.width = 480;
-//       canvas.height = 360;
+      canvas.width = 480;
+      canvas.height = 360;
 
-//       var constraints = {
-//         audio: true,
-//         video: true
-//       };
+      var constraints = {
+        audio: true,
+        video: true
+      };
 
-//       function successCallback(stream) {
-//         window.stream = stream; 
-//         video.srcObject = stream;
-//       }
+      function successCallback(stream) {
+        window.stream = stream; 
+        video.srcObject = stream;
+      }
 
-//       function errorCallback(error) {
-//         console.log('navigator.getUserMedia error: ', error);
-//       }
-//       navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
-//       navigator.getUserMedia(constraints, successCallback, errorCallback);
+      function errorCallback(error) {
+        console.log('navigator.getUserMedia error: ', error);
+      }
+      navigator.getUserMedia = navigator.getUserMedia || navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+      navigator.getUserMedia(constraints, successCallback, errorCallback);
 
 
-//       function convertCanvasToImage(oCanvas) {
-//         imgdata.value=oCanvas.toDataURL();
+      function convertCanvasToImage(oCanvas) {
+        imgdata.value=oCanvas.toDataURL();
 
-//       }
+      }
       
-// }
+}
